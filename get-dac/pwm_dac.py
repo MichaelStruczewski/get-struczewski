d0c1cd1c7b6615612917b03e97e51fa5435d6270
MichaelStruczewski <mikhail.struczewski@icloud.com>
2025-09-30 12:09:51 +0300

    Недоделанное

---
+++ get-dac/pwm_dac.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+class PWM_DAC:
+    def __init__(self, gpio_pin, pwm_freq, dynamic_range, verbose = False):
+        self.gpio_pin = gpio_pin
+        self.pwm_freq = pwm_freq
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_pin, GPIO.OUT, initial = 0)
+
+    def deinit(self):
+        GPIO.output(self.gpio_pin, 0)
+        GPIO.cleanup()
+
+    def decimal2bin(self, value):
+        return [int(element) for element in bin(value)[2:].zfill(8)]
+
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} B)")
+            print("Устанавливаем 0.0 В")
+            voltage = 0
+        voltage = int(voltage / self.dynamic_range * 255)
+        GPIO.output(self.gpio_pin, voltage)
+if __name__ == "__main__":
+    try:
+        dac = PWM_DAC(12, 500, 3.3, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте еще раз\n")
+    finally:
+        dac.deinit()
