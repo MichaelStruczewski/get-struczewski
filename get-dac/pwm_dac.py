c3548447aeb8061d03e48b9eeaf5105f43246f0a
MichaelStruczewski <mikhail.struczewski@icloud.com>
2025-10-07 10:42:07 +0300

    ЦАП на ШИМЕ

--- get-dac/pwm_dac.py
+++ get-dac/pwm_dac.py
@@ -1,16 +1,22 @@
 import RPi.GPIO as GPIO
+import time
 class PWM_DAC:
     def __init__(self, gpio_pin, pwm_freq, dynamic_range, verbose = False):
         self.gpio_pin = gpio_pin
         self.pwm_freq = pwm_freq
         self.dynamic_range = dynamic_range
         self.verbose = verbose
+        self.pwm = None
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_pin, GPIO.OUT, initial = 0)
+        GPIO.setup(self.gpio_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.gpio_pin, self.pwm_freq)
+        self.pwm.start(0)
+
 
     def deinit(self):
-        GPIO.output(self.gpio_pin, 0)
+        if self.pwm:
+            self.pwm.stop()
         GPIO.cleanup()
 
     def decimal2bin(self, value):
@@ -21,11 +27,11 @@ class PWM_DAC:
             print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} B)")
             print("Устанавливаем 0.0 В")
             voltage = 0
-        voltage = int(voltage / self.dynamic_range * 255)
-        GPIO.output(self.gpio_pin, voltage)
+        duty_cycle = (voltage / self.dynamic_range) * 100
+        self.pwm.ChangeDutyCycle(duty_cycle)
 if __name__ == "__main__":
     try:
-        dac = PWM_DAC(12, 500, 3.3, True)
+        dac = PWM_DAC(12, 500, 3.3, True) 
 
         while True:
             try:
